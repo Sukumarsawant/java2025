b8dfeb68ebfe36164cd1f6f481c7c4a7fa6216b3
Sukumarsawant <sawantsukumar@gmail.com>
2025-02-11 18:20:43 +0530

    first program

---
+++ MSE1/simple_interest.java
@@ -0,0 +1,32 @@
+package MSE1.java2025.MSE1;
+	
+
+import java.util.*;
+
+
+
+public class simple_interest {
+	public static double simpleInterest(double p , double r , double t ) {
+		return (p*r*t)/100.;
+	}
+	public static double compoundInterest(double p , double r , double t) {
+		return ((p*Math.pow(1+r/100,t))-p);
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		// TODO Auto-generated method stub
+		double p , r ,t ; 
+		System.out.println("Enter the values of printcipal , ROI and time  period : ");
+		p = sc.nextDouble();
+		r = sc.nextDouble();
+		t = sc.nextDouble();
+		double s = simpleInterest(p,r,t);
+		double c= compoundInterest(p,r,t);
+		System.out.println("Simple interest is : "+s);
+		System.out.println("Compound Interest is : "+String.format("%.2f",c));
+		
+		
+	}
+
+}
